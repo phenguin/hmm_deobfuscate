33635002cba5e5848b65bcb216569f551a8c7ac2
Justin Cullen <jcullen@mit.edu>
2011-12-07 22:33:27 -0500

    Fixed learning of emission probs between words

--- src/deobfuscate.py
+++ src/deobfuscate.py
@@ -9,7 +9,7 @@ from string import punctuation,digits,ascii_lowercase
 
 # Global parameters
 eta = 0.7
-epsilon = None
+tau = None
 
 # Default corpus to use for english language word frequencies
 EL_corpus = '../data/freq_corpus.txt'
@@ -23,8 +23,8 @@ emission_probs_file = '../data/pickled_emission_probs'
 
 prefix_count = {}
 word_count = {}
-def init_counts(corpus=EL_corpus):
 
+def init_counts(corpus=EL_corpus):
   def prefixes(w):
     return (w[:i] for i in range(len(w)+1))
 
@@ -68,7 +68,6 @@ def learn_hmm(dict_path = wordlist, training_inputs = inputs_path,
   # Compute states and state transition probabilities
   for w in words:
     w = w.lower()
-    print w
     w = w[:-1] # remove EOL char
     for i in range( len(w) ): 
       new = w[:i+1]
@@ -102,34 +101,47 @@ def learn_hmm(dict_path = wordlist, training_inputs = inputs_path,
       for char in line[:-1])
   paired = itertools.izip(observed_chars,true_chars)
 
+  def c_to_i(s):
+    if s == 'word_start':
+      return len(ascii_lowercase)
+    else:
+      return ascii_lowercase.index(s)
+
+  def c_from_i(i):
+    if i == len(ascii_lowercase):
+      return 'word_start'
+    else:
+      return ascii_lowercase[i]
+
   def to_index(letter,ob):
-    return ascii_lowercase.index(letter)* num_obs + observations.index(ob) 
+    return c_to_i(letter) * num_obs + observations.index(ob) 
   def from_index(i):
     char_index = i / num_obs
     ob_index = i % num_obs
-    return (ascii_lowercase[char_index],observations[ob_index])
+    return (c_from_i(char_index),observations[ob_index])
 
   # Construct linear programming problem for cvxopt
-  P = matrix(numpy.zeros( (26 * num_obs,26 * num_obs) ),tc='d')
-  q = matrix(numpy.zeros(26 * num_obs),tc='d')
-  G = matrix(numpy.diag([-1] * (26 * num_obs)),tc='d')
-  h = matrix(numpy.zeros(26 * num_obs),tc='d')
-  A = numpy.zeros( (26, 26*num_obs) )
-  b = matrix(numpy.ones(26),tc='d')
+  P = matrix(numpy.zeros( (27 * num_obs,27 * num_obs) ),tc='d')
+  q = matrix(numpy.zeros(27 * num_obs),tc='d')
+  G = matrix(numpy.diag([-1] * (27 * num_obs)),tc='d')
+  h = matrix(numpy.zeros(27 * num_obs),tc='d')
+  A = numpy.zeros( (27, 27*num_obs) )
+  b = matrix(numpy.ones(27),tc='d')
   # construct q
   for o,a in paired:
     if a == '-':
-      print last_a,o
       q[to_index(last_a,o)] += 1
     elif a != ' ':
       if a not in ascii_lowercase: continue
-      print a,o
       q[to_index(a,o)] += 1
       last_a = a
+    else:
+      q[to_index('word_start',o)] += 1
+      last_a = 'word_start'
   q = -q # Invert since we want maximum not minimum
 
   # construct A
-  for i in range(26):
+  for i in range(27):
     for k in range(num_obs):
       A[i][i * num_obs + k] = 1
   A = matrix(A,tc='d')
@@ -142,7 +154,10 @@ def learn_hmm(dict_path = wordlist, training_inputs = inputs_path,
   emission_probs = dict( [(s,{}) for s in states] )
   for s in emission_probs.keys():
     for o in observations:
-      emission_probs[s][o] = sol[to_index(s[-1],o)]
+      if s != 'word_start':
+        emission_probs[s][o] = sol[to_index(s[-1],o)]
+      else:
+        emission_probs[s][o] = sol[to_index(s,o)]
 
   return (tuple(states), observations, trans, emission_probs)
 
@@ -207,3 +222,5 @@ def deobfuscate(s):
     return result
   return ' '.join(split_list([index_states[x] for x in
     a],'word_start'))
+
+
